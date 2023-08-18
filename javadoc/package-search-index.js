d37e1156d3c47d7df82fc7ebf48422f48dc8e36a
Greg Moens <greg_moens@vuzix.com>
2023-08-18 10:41:07 -0400

    0.5 upload

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.vuzix.ultralite"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.vuzix.ultralite"}];updateSearchResults();
